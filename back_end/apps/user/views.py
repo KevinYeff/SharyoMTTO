a38788b9d092292043d518ce14ba14fe1f639a5a
KevinYeff <kevinyeff94@gmail.com>
2024-05-24 14:57:28 -0500

    feat(API-requests): Basic GET request implementation for user's list

--- back_end/apps/user/views.py
+++ back_end/apps/user/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from django.http import HttpResponseNotFound
+from .models import User
+from django.db import DatabaseError
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def listUsers(request: HttpRequest):
+    """Method that will return a list of users from db
+
+    Args:
+        request (HttpRequest): The HTTP request object containing information
+        the incoming request.
+    """
+    if request.method == 'GET':
+        try:
+            users = list(User.objects.all())
+            if not users:
+                return HttpResponseNotFound("No users found", status=204)
+            # return render(request, 'users.html', {'users': users})
+            return JsonResponse(users, safe=False)
+        except DatabaseError as e:
+            return JsonResponse(
+                {
+                    'error': 'Database error: ' + str(e)
+                },
+                status=500)
+        except ObjectDoesNotExist:
+            return JsonResponse({'error': 'No users found'}, status=404)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse(
+            {
+                'error': 'Method not allowed, GET request only'
+            },
+            status=405)
