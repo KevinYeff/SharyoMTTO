3cca4c327e508bba9fb459ac6edf615b69c1e2d1
KevinYeff <kevinyeff94@gmail.com>
2024-05-27 14:52:36 -0500

    feat(API-request): Basic GET implementation to list a user based on ID

--- back_end/apps/user/views.py
+++ back_end/apps/user/views.py
@@ -5,6 +5,7 @@ from .models import User
 from django.db import DatabaseError
 from django.core.exceptions import ObjectDoesNotExist
 
+
 # Create your views here.
 def listUsers(request: HttpRequest):
     """Method that will return a list of users from db
@@ -15,11 +16,13 @@ def listUsers(request: HttpRequest):
     """
     if request.method == 'GET':
         try:
-            users = list(User.objects.all())
+            users = User.objects.all()
+            users_list = list(users)
+
             if not users:
                 return HttpResponseNotFound("No users found", status=204)
             # return render(request, 'users.html', {'users': users})
-            return JsonResponse(users, safe=False)
+            return JsonResponse(users_list, safe=False, status=200)
         except DatabaseError as e:
             return JsonResponse(
                 {
@@ -36,3 +39,39 @@ def listUsers(request: HttpRequest):
                 'error': 'Method not allowed, GET request only'
             },
             status=405)
+
+def listUsersById(request: HttpRequest, id: int):
+    """Method that will return users based on id from the database
+
+    Args:
+        request (HttpRequest): The HTTP object containing information.
+        id (int): The user's ID to search for in the database.
+    Returns:
+        JsonResponse: A JSON response containing the user's information if
+        found, or error message if user does not exist.
+    """
+    if request.method != 'GET':
+        return JsonResponse(
+            {
+                'error': 'Method not allowed, GET request only'
+            }, safe=False, status=405)
+
+    try:
+        user = User.objects.get(id=id)
+        return JsonResponse(user, safe=False, status=200)
+
+    except User.DoesNotExist:
+        return JsonResponse(
+            {
+                'error': 'User not found'
+            }, safe=False, status=404)
+    except DatabaseError as e:
+        return JsonResponse(
+            {
+                'error': 'Database error: ' + str(e)
+            }, safe=False, status=500)
+    except Exception as e:
+        return JsonResponse(
+            {
+                'error': str(e)
+            }, safe=False, status=500)
