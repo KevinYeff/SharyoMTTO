f3b12ccd08e3eefd1632397638f61c4e89e0bab5
KevinYeff <kevinyeff94@gmail.com>
2024-05-27 14:54:08 -0500

    feat(API-endpoint): Route to list user based on ID added

--- back_end/apps/user/urls.py
+++ back_end/apps/user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.listUsers)
+    path('users/', views.listUsers),
+    path('users/<int:id>/', views.listUsersById)
 ]
