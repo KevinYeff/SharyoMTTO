f169a4e6744076b2940e747e655ad4d8f17bc09d
KevinYeff <kevinyeff94@gmail.com>
2024-05-27 23:49:52 -0500

    feat(contact_book-views): Add try, except blocks, and validation for fields in forms

--- back_end/apps/contact_book/views.py
+++ back_end/apps/contact_book/views.py
@@ -1,24 +1,57 @@
 from django.shortcuts import render
 from .forms import StoreForm, WorkshopForm, MechanicForm, ContactForm
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
+
 
 # Create your views here.
 
 def new_contact(request):
-    """Method that will return a form to create a new contact"""
+    """
+    Método que en principio retorna un formulario para crear un nuevo contacto.
+
+    Args:
+        request(objeto): Objeto del módulo HTTP que contiene información acerca
+        de la petición entrante.
+    """
+
     if request.method =='GET':
-        form = ContactForm()
-        context = {
-            'form': form
-        }
-        return render(request, 'new_contact.html', context)
-    
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse("Contacto creado exitoxamente")
-    
+        try:
+            form = ContactForm()
+            context = {
+                'form': form
+            }
+            return render(request, 'new_contact.html', context)
+
+        except Exception as e:
+            return HttpResponse("Error: " + str(e), status=500)
+
+    elif request.method == 'POST':
+        try:
+            form = ContactForm(request.POST)
+
+            if form.is_valid():
+
+                if not form.cleaned_data.get('name') \
+                    or not form.cleaned_data.get('email') \
+                    or not form.cleaned_data.get('phone'):
+                        raise ValidationError('Todos los campos \
+                            son obligatorios')
+
+                form.save()
+                return HttpResponse("Contacto creado exitoxamente")
+
+            else:
+                return HttpResponse("Error al crear contacto", status=400)
+        except ValidationError as e:
+            return HttpResponse("Error: " + str(e), status=400)
+        except Exception as e:
+            return HttpResponse("Error: " + str(e), status=500)
+
+    else:
+        return HttpResponse("Petición inválida", status=400)
+
+
 def new_store(request):
     """Method that will return a form to create a new store"""
     if request.method =='GET':
