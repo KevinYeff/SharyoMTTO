197068cc8ced98dff6a8a5e858045e3c0fb60464
KevinYeff <kevinyeff94@gmail.com>
2024-06-03 15:59:03 -0500

    hotfix(views): script now uses the modified variables in urls.py file

--- back_end/apps/contact_book/views.py
+++ back_end/apps/contact_book/views.py
@@ -10,15 +10,15 @@ class ContactBookListView(generics.ListCreateAPIView):
     serializer_class = ContactBookSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Contact_book.objects.filter(contact__user=user_id)
 
 class ContactBookDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ContactBookSerializer
-    lookup_field = 'id'
+    lookup_field = 'cb_id'
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Contact_book.objects.filter(contact__user=user_id)
 
 
@@ -28,15 +28,15 @@ class ContactListView(generics.ListCreateAPIView):
     serializer_class = ContactSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Contact.objects.filter(contact_book__contact__user=user_id)
 
 class ContactDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ContactSerializer
-    lookup_field = 'id'
+    lookup_field = 'contact_id'
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Contact.objects.filter(contact_book__contact__user=user_id)
 
 # Store Views
@@ -45,15 +45,15 @@ class StoreListView(generics.ListCreateAPIView):
     serializer_class = StoreSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Store.objects.filter(contact_book__contact_user=user_id)
 
 class StoreDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = StoreSerializer
-    lookup_field = 'id'
+    lookup_field = 'store_id'
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Store.objects.filter(contact_book__contact__user=user_id)
 
 # Workshop Views
@@ -62,15 +62,15 @@ class WorkshopListView(generics.ListCreateAPIView):
     serializer_class = WorkshopSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Workshop.objects.filter(contact_book__contact__user=user_id)
 
 class WorkshopDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = WorkshopSerializer
-    lookup_field = 'id'
+    lookup_field = 'workshop_id'
 
     def get_query_set(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Workshop.objects.filter(contact_book__contact__user=user_id)
 
 # Mechanic Views
@@ -79,13 +79,13 @@ class MechanicListView(generics.ListCreateAPIView):
     serializer_class = MechanicSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Mechanic.objects.filter(contact_book__contact__user=user_id)
 
 class MechanicDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MechanicSerializer
-    lookup_field = 'id'
+    lookup_field = 'mechanic_id'
 
     def get_queryset(self):
-        user_id = self.kwargs['id']
+        user_id = self.kwargs['user_id']
         return Mechanic.objects.filter(contact_book__contact__user=user_id)
