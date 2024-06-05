bed6d8e1dbf40824fbcc1d440e14cf3ccd978d75
tomasguell <tomasguell123@gmail.com>
2024-06-05 16:20:20 -0300

    Children div styleado

--- front_end/sharyo_mtto/src/app/dashboard/layout.jsx
+++ front_end/sharyo_mtto/src/app/dashboard/layout.jsx
@@ -2,11 +2,15 @@ import SideNav from "@/componentes_propios/dashboard/dashboard";
 
 export default function Layout({ children }) {
     return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
+        <div className="flex h-screen flex-col md:flex-row">
+            <div className="flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+            <div className="flex-grow p-4 md:p-6">
+                <div className="bg-gray-100 h-full w-full mx-auto rounded-lg p-5 shadow-md">
+                    {children}
+                </div>
+            </div>
         </div>
     );
 }
