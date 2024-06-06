3a90ec4c65e7e313e30957810c9d9048c1e6fc6c
KevinYeff <kevinyeff94@gmail.com>
2024-06-06 18:41:57 -0500

    hofix(builder): configuration matches path now

--- back_end/build.sh
+++ back_end/build.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r back_end/requierements.txt
+pip install -r requierements.txt
 
 # Convert static asset files
-python back_end/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python back_end/manage.py migrate
+python manage.py migrate
