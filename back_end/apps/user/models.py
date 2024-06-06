1fe8d8133be89b6e3079ce48147e95adbde7f05a
tomasguell <tomasguell123@gmail.com>
2024-06-06 00:14:45 -0300

    Hotfix (Admin) arreglo para entrar al admin

--- back_end/apps/user/models.py
+++ back_end/apps/user/models.py
@@ -7,64 +7,68 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your models here.
 
+
 class Country(models.Model):
     country = models.CharField(max_length=50)
     shortname = models.CharField(max_length=3, blank=True)
     phonecode = models.CharField(max_length=7, blank=True)
-    
+
     def __str__(self):
         return self.country
-    
+
     class Meta:
-        db_table = 'pais'
-        verbose_name = 'Pais'
-        verbose_name_plural = 'paises'
+        db_table = "pais"
+        verbose_name = "Pais"
+        verbose_name_plural = "paises"
+
 
 class State(models.Model):
     state = models.CharField(max_length=50)
     id_country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.state
-    
+
     class Meta:
-        db_table = 'estados'
-        verbose_name = 'Estados'
-        verbose_name_plural = 'estados'
+        db_table = "estados"
+        verbose_name = "Estados"
+        verbose_name_plural = "estados"
+
 
 class City(models.Model):
     city = models.CharField(max_length=50)
     id_state = models.ForeignKey(State, on_delete=models.CASCADE, blank=True)
-    
+
     def __str__(self):
         return self.city
-    
+
     class Meta:
-        db_table = 'ciudad'
-        verbose_name = 'Ciudad'
-        verbose_name_plural = 'ciudades'
-        
-        
- # *** SECCION USUARIO ***  
+        db_table = "ciudad"
+        verbose_name = "Ciudad"
+        verbose_name_plural = "ciudades"
+
+
+# *** SECCION USUARIO ***
 
 # Modelo para la tabla de "usuario"
-     
-class User(AbstractBaseUser):
-    
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
     # Informacion personal
-    name = models.CharField('Nombre', max_length=20)
-    last_name = models.CharField('Apellido', max_length=20)
-    mobile = models.CharField('Celular', max_length=20, unique=True)
+    name = models.CharField("Nombre", max_length=20)
+    last_name = models.CharField("Apellido", max_length=20)
+    mobile = models.CharField("Celular", max_length=20, unique=True)
     phone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField('Correo electronico', unique= True)
+    email = models.EmailField("Correo electronico", unique=True)
     birth_date = models.DateField(default=timezone.now())
     country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True)
     state = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
-    
+
     # Informacion de usuario
-    username = models.CharField('Nombre de usuario', unique=True, max_length=20)
-    
+    username = models.CharField("Nombre de usuario", unique=True, max_length=20)
+
     # Informacion de accesos
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -73,35 +77,35 @@ class User(AbstractBaseUser):
 
     date_joined = models.DateTimeField(default=timezone.now())
     last_login = models.DateTimeField(default=timezone.now())
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'name', 'last_name', 'mobile']
-    
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "name", "last_name", "mobile"]
+
     objects = UserManager()
-    
+
     def __str__(self):
-        return f'Usuario {self.username}'
-    
+        return f"Usuario {self.username}"
+
     @property
     def get_full_name(self):
-        return f'{self.name} {self.last_name}'
-    
+        return f"{self.name} {self.last_name}"
+
     def tokens(self):
         refresh_token = RefreshToken.for_user(self)
         return {
-            'refresh': str(refresh_token),
-            'access': str(refresh_token.access_token)    
+            "refresh": str(refresh_token),
+            "access": str(refresh_token.access_token),
         }
-        
+
     class Meta:
-        db_table = 'usuario'
-        verbose_name = 'Usuario'
-        verbose_name_plural = 'Usuarios'
-        
+        db_table = "usuario"
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
 
 class OneTimePassword(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=6, unique=True)
-    
+
     def __str__(self):
-        return f'{self.user.name} - passcode'
+        return f"{self.user.name} - passcode"
