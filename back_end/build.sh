4463211bc1bb3f5ec18a9ae6cbd652aedc92abc0
KevinYeff <kevinyeff94@gmail.com>
2024-06-11 22:13:44 -0500

    hotfix(builder): restored builder

--- back_end/build.sh
+++ back_end/build.sh
@@ -5,23 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requierements.txt
 
-# Clean up old migrations
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc" -delete
-
-# Drop and recreate the database (specific to PostgreSQL)
-PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -U $DATABASE_USER -c "DROP DATABASE IF EXISTS $DATABASE_NAME;"
-PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -U $DATABASE_USER -c "CREATE DATABASE $DATABASE_NAME;"
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py makemigrations user contact_book vehicles
 python manage.py migrate
-
-
-# Create a superuser if none exists
-if [ -z "$(python manage.py shell -c 'from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).count())')" ]; then
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
-fi
