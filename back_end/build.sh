e318b9979e7eb86f7c7b19f3996e59224dd21960
KevinYeff <kevinyeff94@gmail.com>
2024-06-11 21:55:35 -0500

    hotfix(builder): Clean migrations, added a superuser

--- back_end/build.sh
+++ back_end/build.sh
@@ -5,6 +5,15 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requierements.txt
 
+# Clean up old migrations
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc" -delete
+
+# Delete local SQLite database if it exists (adjust if needed for your setup)
+if [ -f "db.sqlite3" ]; then
+    rm db.sqlite3
+fi
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
@@ -13,3 +22,9 @@ python manage.py makemigrations user
 python manage.py makemigrations contact_book
 python manage.py makemigrations vehicles
 python manage.py migrate
+
+
+# Create a superuser if none exists
+if [ -z "$(python manage.py shell -c 'from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).count())')" ]; then
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+fi
