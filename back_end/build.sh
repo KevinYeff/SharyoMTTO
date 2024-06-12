cbb354e08f1f25752fe41dcb46dc8d4453f031e7
KevinYeff <kevinyeff94@gmail.com>
2024-06-11 22:27:20 -0500

    hotfix(migrations-builder): clean migrations , builder will apply migrations test

--- back_end/build.sh
+++ back_end/build.sh
@@ -9,5 +9,5 @@ pip install -r requierements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py makemigrations user contact_book vehicles
+python manage.py makemigrations
 python manage.py migrate
